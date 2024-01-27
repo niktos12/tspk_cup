5f37443d14e398ddfabfe1237c7e1dc36ef59369
niktos12 <nikitosss121207@gmail.com>
2024-01-27 20:23:44 +0400

    ....

--- my-app/src/components/Modal.tsx
+++ my-app/src/components/Modal.tsx
@@ -21,24 +21,24 @@ export function Modal () {
           <IoCloseCircleOutline onClick={closeModal} className='cursor-pointer text-[#3773FF] w-[39px] h-[39px] hover:text-[#0D0D0E] duration-300'/>
         </div>
         <div className='flex flex-row justify-between'>
-          <input placeholder='Имя' className='border-b-2 border-black p-6 pb-1 pl-1 bg-transparent w-[48%] text-[#3773FF] font-semibold'/>
-          <input placeholder='Фамилия' className='border-b-2 border-black p-6 pb-1 pl-1 bg-transparent w-[48%] text-[#3773FF] font-semibold'/>
+          <input placeholder='Имя' className='border-b-2 border-black p-6 pb-1 pl-1 bg-transparent w-[48%] text-[#3773FF] font-black'/>
+          <input placeholder='Фамилия' className='border-b-2 border-black p-6 pb-1 pl-1 bg-transparent w-[48%] text-[#3773FF] font-black'/>
         </div>
         <div className='flex flex-col gap-8'>
           <div className='flex flex-col'>
             <p className=''>Ваш телеграм</p>
-            <input placeholder='@TSPKCup' className='border-b-2 border-black p-6 py-1 pl-1 bg-transparent text-[#3773FF] font-semibold'/>
+            <input placeholder='@TSPKCup' className='border-b-2 border-black p-6 py-1 pl-1 bg-transparent text-[#3773FF] font-black'/>
           </div>
           <div className='flex flex-col'>
             <p className=''>Группа</p>
-            <input placeholder='ИСиП-11' className='border-b-2 border-black p-6 py-1 pl-1 bg-transparent text-[#3773FF] font-semibold'/>
+            <input placeholder='ИСиП-11' className='border-b-2 border-black p-6 py-1 pl-1 bg-transparent text-[#3773FF] font-black'/>
           </div>
           <div className='flex flex-col'>
             <p className=''>Команда</p>
-            <input placeholder='NE DOGONYAT' className='border-b-2 border-black p-6 py-1 pl-1 bg-transparent text-[#3773FF] font-semibold'/>
+            <input placeholder='NE DOGONYAT' className='border-b-2 border-black p-6 py-1 pl-1 bg-transparent text-[#3773FF] font-black'/>
           </div>
           <div className='flex flex-col'>
-            <button className='py-4 px-6 bg-[#3773FF] text-[#FFFFFF] rounded-3xl text-3xl font-semibold'>Отправить</button>
+            <button className='py-4 px-6 bg-[#3773FF] text-[#FFFFFF] rounded-3xl text-3xl font-black'>Отправить</button>
             <p className=''>Нажимая "Отправить", вы принимаете политику хранения и обработки <a href='https://piybeep.com/privacy_policy.pdf'>персональных данных</a></p>
           </div>
            
